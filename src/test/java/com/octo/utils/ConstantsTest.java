cf288d2358cf99bd7940d7dd86475b4cf66ba58d
Vincent Moittié <vincent.moittie@arkena.com>
2019-06-06 14:30:19 +0200

    Add default constantes

---
+++ src/test/java/com/octo/utils/ConstantsTest.java
@@ -0,0 +1,25 @@
+package com.octo.utils;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.lang.reflect.Constructor;
+
+import org.junit.Test;
+
+public class ConstantsTest {
+
+    @Test
+    public void testConstructor() {
+        Exception exception = null;
+        try {
+            final Constructor<Constants> c = Constants.class.getDeclaredConstructor();
+            c.setAccessible(true);
+            c.newInstance();
+        } catch (final Exception e) {
+            exception = e;
+        }
+        assertNotNull(exception);
+        assertEquals(UnsupportedOperationException.class, exception.getCause().getClass());
+    }
+}

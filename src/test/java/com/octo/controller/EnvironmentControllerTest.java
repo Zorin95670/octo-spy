70f5a0818ed88fdad5d5bcfc8ca3fd3efc50ab7a
Vincent Moittié <vincent.moittie@arkena.com>
2019-06-11 17:06:12 +0200

    Improve Environment controller

--- src/test/java/com/octo/controller/EnvironmentControllerTest.java
+++ src/test/java/com/octo/controller/EnvironmentControllerTest.java
@@ -3,6 +3,9 @@ package com.octo.controller;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
@@ -10,25 +13,32 @@ import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.test.JerseyTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.octo.controller.EnvironmentController;
-import com.octo.controller.InfoController;
+import com.octo.model.dto.environment.EnvironmentDTO;
+import com.octo.service.EnvironmentService;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(locations = { "classpath:application-context.xml" })
 public class EnvironmentControllerTest extends JerseyTest {
 
-    @Autowired
-    InfoController controller;
+    @Mock
+    EnvironmentService service;
+
+    @InjectMocks
+    EnvironmentController controller;
 
     @Override
     protected Application configure() {
+        MockitoAnnotations.initMocks(this);
         final ResourceConfig rc = new ResourceConfig(EnvironmentController.class);
 
         rc.property("contextConfigLocation", "classpath:application-context.xml");
@@ -38,10 +48,13 @@ public class EnvironmentControllerTest extends JerseyTest {
 
     @Test
     public void getAllTest() throws JsonProcessingException {
+        List<EnvironmentDTO> environments = new ArrayList<>();
+        environments.add(new EnvironmentDTO());
+        Mockito.when(this.service.findAll()).thenReturn(environments);
         final Response response = this.target("/environment").request().get();
 
         assertEquals(HttpStatus.OK.value(), response.getStatus());
-        ArrayNode environments = response.readEntity(ArrayNode.class);
-        assertNotEquals(4, environments.size());
+        ArrayNode array = response.readEntity(ArrayNode.class);
+        assertNotEquals(1, array.size());
     }
 }
